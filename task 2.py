c37583b3fd12380cee1408659f266eb8732c2092
Mallesh-00 <25bcs091@iiitdwd.ac.in>
2025-11-26 21:37:54 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,4 @@
+n1 = input("Enter first name: ")
+n2 = input("Enter last name: ")
+concat = n1.capitalize() + " " + n2.capitalize()
+print(f"Hello, {concat}! Welcome to the  Python program.")
---
+++ task1.py
@@ -0,0 +1,10 @@
+n1 = float(input("Enter first number: "))
+n2 = float(input("Enter second number: "))
+sum = n1 + n2
+print(" addition :", sum)
+diff = n1 - n2
+print(" subtraction :", diff)
+prod = n1 * n2  
+print(" multiplication :", prod)
+quot = n1 / n2
+print("division :", quot)
